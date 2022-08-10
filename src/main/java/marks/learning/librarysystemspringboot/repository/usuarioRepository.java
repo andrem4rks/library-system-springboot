9cf9f816d9d02bd7d93ae4c46db61f01066371a1
Andre <andremarks867@gmail.com>
2022-08-10 16:12:31 -0300

    cria a extencao de jpa

---
+++ src/main/java/marks/learning/librarysystemspringboot/repository/usuarioRepository.java
@@ -0,0 +1,7 @@
+package marks.learning.librarysystemspringboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface usuarioRepository extends JpaRepository<Usuario , Long> {
+    
+}
