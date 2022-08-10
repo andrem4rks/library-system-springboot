b2c0e6526baddf73154737ea320b761c7bd98485
Andre <andremarks867@gmail.com>
2022-08-10 14:38:46 -0300

    cria o projeto e repositorio

---
+++ src/main/java/marks/learning/librarysystemspringboot/LibrarySystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package marks.learning.librarysystemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marks/learning/librarysystemspringboot/LibrarySystemSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package marks.learning.librarysystemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
